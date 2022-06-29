c19a8b0ca6fae89a3c93831ac55b291c55a261e0
Fomegne <fomegnemeudje@outlook.com>
2022-06-29 14:03:11 +0100

    Add token authentication

--- backend/mayaschool/settings.py
+++ backend/mayaschool/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'core.apps.CoreConfig',
     'students.apps.StudentsConfig',
     'staffs.apps.StaffsConfig',
@@ -137,3 +138,9 @@ SESSION_COOKIE_AGE = 10800
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
--- backend/mayaschool/urls.py
+++ backend/mayaschool/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-token-auth/', obtain_auth_token),
     path('core/', include('core.urls')),
     path('students/', include('students.urls')),
     path('staffs/', include('staffs.urls')),
