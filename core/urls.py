2631330c788e431adc6c0a5f88e6437742989ae4
Fomegne <fomegnemeudje@outlook.com>
2022-07-11 09:55:14 +0100

    Update (core): Add dashboard page

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
-from django.urls import include, path
+from django.urls import path
 from . import views
 
 
 # Wire up our API using automatic URL routing.
 urlpatterns = [
-
+    path('', views.index, name='dashboard')
 ]
