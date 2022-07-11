ef8eaeb9ae8078ba1b2bd47d77d5613c892f8425
Fomegne <fomegnemeudje@outlook.com>
2022-07-11 14:06:54 +0100

    add students list

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,9 @@
-from django.urls import include, path
+from django.urls import path
 from . import views
 
 
 # Wire up our API using automatic URL routing.
 urlpatterns = [
-
+    path('', views.index, name='students_list'),
+    path('<int:pk>', views.details, name='student_details'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,2 +1,16 @@
 from core.models import AcademicSession
 from .models import Student, ClassStudent
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'students/students_list.html', {
+        'title': 'Students',
+        'class_students': ClassStudent.objects.filter(session=AcademicSession.get_current())
+    })
+
+
+def details(request):
+    return render(request, 'students/student_details.html', {
+        'title': 'Student Details'
+    })
