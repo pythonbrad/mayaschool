0f46b92fc069907e9fec23e5d0f5114dd2e71b31
Fomegne <fomegnemeudje@outlook.com>
2022-08-01 13:38:51 +0100

    Update (core)

--- mayaschool/settings.py
+++ mayaschool/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'core.apps.CoreConfig',
     'students.apps.StudentsConfig',
     'staffs.apps.StaffsConfig',
